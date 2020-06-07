5f7849b4608e3fe4c9296704ed1b9b5111bf0728
luisfersk <39956205+luisfersk@users.noreply.github.com>
2020-06-06 20:43:00 -0500

    LISTO

--- php/coneccion.php
+++ php/coneccion.php
@@ -5,7 +5,7 @@ function conectarse()
   $servername = "127.0.0.1";
   $dbname     = "mid_db";
   $username   = "root";
-  $password   = "";
+  $password   = "Soka1212Fer";
   $conn       = new mysqli($servername, $username, $password, $dbname);
 
   if ($conn->connect_error) {
